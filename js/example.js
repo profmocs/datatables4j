497bfe7a9b3f09b7b0f0064533e85892abc4b39c
Thibault Duchateau <thibault.duchateau@gmail.com>
2012-09-30 14:53:51 -0700

    Update site for 0.3.0-SNAPSHOT

---
+++ js/example.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	var oTable = $('#exampleTable').dataTable({
+		"aaSorting": []
+	});	
+});
--- js/tabletag.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	var oTable = $('#myTable').dataTable({
-		"bInfo" : false,
-		"bPaginate" : false,
-		"bLengthChange" : false,
-		"aaSorting": []
-	});
-	new FixedHeader( oTable );
-});
